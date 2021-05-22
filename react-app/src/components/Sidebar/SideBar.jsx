fdf2bcbbb9eba7abcfd9bebbac2087ae38c11aef
fabian <koe7the@gmail.com>
2021-05-22 18:47:26 -0400

    Create Sidebar componen with its styles
    This component have inside the searchbar and the content list,
    the history feature its not implemented yet

---
+++ react-app/src/components/Sidebar/SideBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./SideBar.scss";
+import data from "../../content/placeholder.json";
+import SearchBar from "../SearchBar/SearchBar";
+import ContentList from "../ContentList/ContentList";
+
+const SideBar = () => {
+  return (
+    <nav className="sidebar">
+      <SearchBar />
+      {data.data.map((item) => (
+        <ContentList key={item.theme} content={item} />
+      ))}
+    </nav>
+  );
+};
+
+export default SideBar;
