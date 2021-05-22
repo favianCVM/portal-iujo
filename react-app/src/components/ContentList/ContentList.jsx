471c0b7ff0cbea4dd75b9a5f67283a6c39260d9a
fabian <koe7the@gmail.com>
2021-05-22 16:10:13 -0400

    Create Content list component with its styles
    This component display the list of themes so the user can use
    it as an index

---
+++ react-app/src/components/ContentList/ContentList.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { HashLink as Link } from "react-router-hash-link";
+import "./ContentList.scss";
+
+const ContentList = ({ content }) => {
+  const [elements, setElements] = useState([]);
+
+  useEffect(() => {
+    let listElement = content.content.reduce((accumulator, item) => {
+      accumulator.push(
+        <li key={item.title} className="content-list__element">
+          <Link className="content-list__link" to={item.url}>
+            {item.title}
+          </Link>
+        </li>
+      );
+
+      if (item.subContent.length) {
+        accumulator.push(
+          <ul
+            key={`sub-content${item.title}`}
+            className="content-list__sub-list"
+          >
+            {item.subContent.reduce((accumulator, item) => {
+              accumulator.push(
+                <li key={item.title} className="content-list__element">
+                  {item.title}
+                </li>
+              );
+              return accumulator;
+            }, [])}
+          </ul>
+        );
+      }
+
+      return accumulator;
+    }, []);
+
+    setElements([listElement]);
+  }, []);
+
+  return (
+    <ul className="content-list">
+      {
+        <h2 key={content.theme} className="content-list__title">
+          {content.theme}
+        </h2>
+      }
+      {elements}
+    </ul>
+  );
+};
+
+export default ContentList;
