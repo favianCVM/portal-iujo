d265806e3371bec275ffc803a53410e3e9699e5e
fabian <koe7the@gmail.com>
2021-05-24 18:55:38 -0400

    Modify the searchbar component
    Now when the user erase all the search the results disapear

--- react-app/src/components/SearchBar/SearchBar.jsx
+++ react-app/src/components/SearchBar/SearchBar.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HashLink as Link } from "react-router-hash-link";
 import "./SearchBar.scss";
 
 const SearchBar = ({ data }) => {
   const [options, setOptions] = useState(null);
+  const [query, setQuery] = useState("");
   const dataArr = [...data[0].content, ...data[1].content];
 
   const getOptions = (e) => {
-    let query = e.target.value.toLowerCase().trim();
-    if (!query) return setOptions(null);
+    setQuery(e.target.value.toLowerCase().trim());
 
     setOptions(
       dataArr.reduce((accumulator, item) => {
         if (item.title.toLowerCase().includes(query)) {
           accumulator.push(
             <li key={item.title} className="searchbar__result">
-              <Link className="searchbar__result-link" to={item.url}>
+              <Link
+                className="searchbar__result-link"
+                onClick={() => {
+                  setOptions(null);
+                  setQuery("");
+                }}
+                to={`/theme/${item.url}`}
+              >
                 {item.title}
               </Link>
             </li>
@@ -34,8 +41,9 @@ const SearchBar = ({ data }) => {
         type="text"
         placeholder="Buscar un tema"
         className="searchbar"
+        value={query}
       />
-      <ul className="searchbar__result-list">{options}</ul>
+      <ul className="searchbar__result-list">{query && options}</ul>
     </div>
   );
 };
