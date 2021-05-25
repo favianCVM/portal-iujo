aae8cbbbde1e742a53587e276269eafc5d285b48
fabian <koe7the@gmail.com>
2021-05-24 20:46:05 -0400

    Implement container in the views
    This create the routes and implement the containers in it

--- react-app/src/services/Router.jsx
+++ react-app/src/services/Router.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-import Placeholder from "../components/Placeholder";
+import HomeContainer from "../containers/HomeContainer";
+import ThemeContainer from "../containers/ThemeContainer";
+import TermGlossaryContainer from "../containers/TermGlossaryContainer";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Placeholder} />
+        <Route exact path="/" component={HomeContainer} />
+        <Route exact path="/theme/:title" component={ThemeContainer} />
+        <Route exact path="/term_glosary" component={TermGlossaryContainer} />
       </Switch>
     </BrowserRouter>
   );
