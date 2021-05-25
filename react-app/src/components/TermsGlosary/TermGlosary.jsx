b96a4e44d3555c5e5f69de79eff712f5f572ea9f
fabian <koe7the@gmail.com>
2021-05-24 20:23:44 -0400

    Create term glosary component
    Here the user will search a term definition to
    see it

---
+++ react-app/src/components/TermsGlosary/TermGlosary.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "./TermsGlosary.scss";
+import Term from "../Term/Term";
+import TermSearchBar from "../TermSearchBar/TermSearchBar";
+import { data } from "../../content/termsPlaceholder.json";
+
+const TermsGlosary = () => {
+  const [terms, setTerms] = useState([]);
+  const [selectedTerm, setSelectedTerm] = useState(null);
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+  };
+
+  useEffect(() => {
+    let termsArray = shuffleArray(data);
+    setTerms(termsArray.slice(0, 9));
+  }, []);
+
+  return (
+    <div className="term-glosary">
+      <TermSearchBar terms={data} setSelectedTerm={setSelectedTerm} />
+      <div className="term__container">
+        {selectedTerm
+          ? selectedTerm
+          : terms.map((item) => <Term key={item.title} content={item} />)}
+      </div>
+    </div>
+  );
+};
+
+export default TermsGlosary;
