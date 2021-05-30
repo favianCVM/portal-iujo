b837bb13e5fd7c843a26176fd5c7102e6b74cd32
fabian <koe7the@gmail.com>
2021-05-30 03:08:51 -0400

    Feature home page scroll to top

--- react-app/src/containers/HomeContainer.jsx
+++ react-app/src/containers/HomeContainer.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header/Header";
 import SideBar from "../components/Sidebar/SideBar";
 import Footer from "../components/Footer/Footer";
 
 const HomeContainer = () => {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, []);
   return (
     <div className="container">
       <Header />
